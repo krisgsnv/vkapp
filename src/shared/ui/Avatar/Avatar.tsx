df272568d51686fd2911390260b5d9e52a87fc4c
krisgsnv <krigasf@gmail.com>
2024-03-08 01:20:06 +0300

    feat: avatar component

---
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -0,0 +1,9 @@
+import style from "./style.module.css";
+
+interface IAvatarProps {
+    color: string;
+}
+
+export const Avatar = ({ color }: IAvatarProps) => {
+    return <div className={style.avatar} style={{backgroundColor: color}}></div>;
+};
